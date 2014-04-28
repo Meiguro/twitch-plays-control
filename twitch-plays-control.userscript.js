6107d2b2ed3ef9d3fe5df7d3fa468ba2720e540a
Meiguro <meiguro@meiguro.com>
2014-04-27 22:37:59 -0700

    Add settings box with enabled and show border checkboxes

--- twitch-plays-control.userscript.js
+++ twitch-plays-control.userscript.js
@@ -10,17 +10,19 @@ var config = Control.config = {
     scale: 0.444,
     size: [256, 192],
     barHeight: 30
-  }
+  },
+  enabled: true,
+  showBorder: true
 };
 
 var $player = $('.dynamic-player');
-var $mouseBox = $('<div/>').addClass('mouse-box');
+var $chatSettings = $('.js-chat-settings');
+var $mouseBox = $('<div/>').addClass('tpc-mouse-box');
+var $controlSettings = $('<div/>').addClass('tpc-control-settings');
 
-Control.update = function() {
-  if (!$.contains($player[0], $mouseBox[0])) {
-    clearInterval(Control.interval);
-  }
+var State = Control.State = {};
 
+Control.updateMouseBox = function() {
   var playerWidth = $player.width();
   var playerHeight = $player.height() - config.screen.barHeight;
 
@@ -28,7 +30,7 @@ Control.update = function() {
       Control.lastPlayerHeight === playerHeight) {
     return;
   }
-  
+
   Control.lastPlayerWidth = playerWidth;
   Control.lastPlayerHeight = playerHeight;
 
@@ -55,17 +57,103 @@ Control.update = function() {
   });
 };
 
+Control.updateControlSettings = function() {
+  if (!$chatSettings.is(':visible')) {
+    return;
+  }
+
+  var $child = $chatSettings.find(':first');
+
+  $controlSettings.css({
+    position: 'absolute',
+    minWidth: $child.width(),
+    right: $child.outerWidth() + 5,
+    bottom: 0
+  });
+};
+
+Control.update = function() {
+  if (!$.contains($player[0], $mouseBox[0])) {
+    clearInterval(Control.interval);
+  }
+
+  Control.updateMouseBox();
+  Control.updateControlSettings();
+};
+
 Control.onClick = function(e) {
   var x = Math.round(e.offsetX / Control.scale);
   var y = Math.round(e.offsetY / Control.scale);
   $('.ember-text-area').val(x + ',' + y);
 };
 
+var makeCheckbox = function(id, label, onChange, value) {
+  var $elem = $('<p><label for="'+id+'"><input id="'+id+'" type="checkbox">'+label+'</label></p>');
+  var $input = $elem.find('input').prop('checked', value).on('change', onChange);
+  onChange.call($input, onChange);
+  return $elem;
+};
+
+Control.saveConfig = function() {
+  localStorage.TPControl = JSON.stringify(config);
+};
+
+Control.onChangeEnable = function(e) {
+  var newValue = config.enabled = $(this).is(':checked');
+  $mouseBox.css({
+    display: newValue ? 'block' : 'none'
+  });
+  Control.saveConfig();
+};
+
+Control.onChangeBorder = function(e) {
+  var newValue = config.showBorder = $(this).is(':checked');
+  $mouseBox.css({
+    border: newValue ? '2px solid rgba(255, 255, 255, 0.5)' : 'none'
+  });
+  Control.saveConfig();
+};
+
 Control.init = function() {
-  $player.find('.mouse-box').remove();
+  $('.tpc-mouse-box').remove();
+  $('.tpc-control-settings').remove();
+
+  if (!localStorage.TPControl) {
+    localStorage.TPControl = Control.config;
+  } else {
+    try {
+      lastConfig = JSON.parse(localStorage.TPControl);
+      for (var k in lastConfig) {
+        config[k] = lastConfig[k];
+      }
+    } catch(e) {
+      console.log(e);
+    }
+  }
+  Control.saveConfig();
+
   $player.css({ position: 'relative' });
-  $mouseBox.css({ border: '2px solid rgba(150, 150, 150, 0.2)' });
+  $chatSettings.css({ position: 'absolute' });
+
+  $mouseBox.css({
+    cursor: 'pointer',
+    border: '2px solid rgba(150, 150, 150, 0.2)'
+  });
+
+  $controlSettings.append(
+    '<div class="chat-menu">' +
+      '<div class="chat-menu-header">Touch Control Settings</div>' +
+      '<div class="chat-menu-content tpc-control-checkboxes"></div>' +
+    '</div>');
+
+  $controlSettings.find('.tpc-control-checkboxes')
+    .append(State.enabledCheckbox = makeCheckbox(
+      'tpc-enabled-checkbox', 'Enable touch control', Control.onChangeEnable, config.enabled))
+    .append(State.borderCheckbox = makeCheckbox(
+      'tpc-border-checkbox', 'Show border box', Control.onChangeBorder, config.showBorder));
+
   $player.append($mouseBox);
+  $chatSettings.append($controlSettings);
 
   $mouseBox.on('click', Control.onClick);
 
