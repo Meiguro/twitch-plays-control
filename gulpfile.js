3c9463b34bf1b2d6b78f4b13656b4d678f8ea13a
Meiguro <meiguro@meiguro.com>
2014-05-13 17:59:46 -0700

    Move source files to src directory

--- gulpfile.js
+++ gulpfile.js
@@ -4,14 +4,18 @@ var browserify = require('gulp-browserify');
 var concat = require('gulp-concat');
 var header = require('gulp-header');
 
+var pkg = 'twitch-plays-control';
+var metafile = pkg + '.meta.js';
+var userfile = pkg + '.user.js';
+
 gulp.task('scripts', function() {
-  gulp.src(['control.js'])
-      .pipe(browserify({ paths: ['.'] }))
-      .pipe(concat('twitch-plays-control.user.js'))
-      .pipe(header(fs.readFileSync('intro.js', 'utf8')))
-      .pipe(gulp.dest('.'))
+  gulp.src(['src/control.js'])
+      .pipe(browserify({ paths: ['src'] }))
+      .pipe(concat(userfile))
+      .pipe(header(fs.readFileSync(metafile, 'utf8')))
+      .pipe(gulp.dest('.'));
 });
 
 gulp.task('default', function() {
-  gulp.watch('*.js', ['scripts']);
+  gulp.watch([metafile, 'src/*.js'], ['scripts']);
 });
