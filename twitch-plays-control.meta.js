42408ed0b723b0c36e41f2a2f9703de5fd5d5897
Meiguro <meiguro@meiguro.com>
2014-05-14 02:19:40 -0700

    Bump version to v0.2.6

--- twitch-plays-control.meta.js
+++ twitch-plays-control.meta.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @id             twitch-plays-control@meiguro.com
 // @name           Twitch Plays Pokémon Touch Controller
-// @version        0.2.5
+// @version        0.2.6
 // @author         Meiguro <meiguro@meiguro.com> http://meiguro.com/
 // @namespace      https://github.com/Meiguro/twitch-plays-control
 // @description    Add Touch controls to Twitch Plays Pokemon touch-enabled games.
