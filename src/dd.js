af02527b54f145565747b37a07c4c4ff4eebe770
Meiguro <meiguro@meiguro.com>
2014-05-13 18:33:58 -0700

    Cleanup dd

--- src/dd.js
+++ src/dd.js
@@ -1,32 +1,32 @@
 var dd = {};
 
 dd.last = function(a) {
-    return a[a.length - 1];
-}
+  return a[a.length - 1];
+};
 
 dd.toKey = function(obj, key) {
-    return typeof obj === 'array' ? parseInt(key) : key;
-}
+  return obj instanceof Array ? parseInt(key) : key;
+};
 
 dd.getByKeys = function(obj, keys, offset) {
-    for (var i = 0, ii = keys.length - (offset || 0); i < ii; ++i) {
-        obj = obj[dd.toKey(obj, keys[i])];
-    }
-    return obj;
-}
+  for (var i = 0, ii = keys.length - (offset || 0); i < ii; ++i) {
+    obj = obj[dd.toKey(obj, keys[i])];
+  }
+  return obj;
+};
 
 dd.setByKey = function(obj, key, value) {
-    obj[dd.toKey(obj, key)] = value;
-}
+  obj[dd.toKey(obj, key)] = value;
+};
 
 dd.set = function(obj, ref, value) {
-    var keys = ref.split('.');
-    dd.setByKey(dd.getByKeys(obj, keys, 1), dd.last(keys), value);
-}
+  var keys = ref.split('.');
+  dd.setByKey(dd.getByKeys(obj, keys, 1), dd.last(keys), value);
+};
 
 dd.get = function(obj, ref) {
-    return dd.getByKeys(obj, ref.split('.'));
-}
+  return dd.getByKeys(obj, ref.split('.'));
+};
 
 dd.bindGetSet = function($elem, get, set) {
   $elem.$get = get;
