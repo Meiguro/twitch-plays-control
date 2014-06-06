0d79d2b3bbe292584d2d3c3b3681be605f5de2ae
Meiguro <meiguro@meiguro.com>
2014-06-06 07:43:47 -0700

    Add a space to checkbox labels

--- src/dd-ui.js
+++ src/dd-ui.js
@@ -3,7 +3,7 @@ var dd = require('dd');
 dd.ui = dd.ui || {};
 
 dd.ui.checkbox = function(id, label, obj, ref, onChange) {
-  var $elem = $('<p><label for="'+id+'"><input id="'+id+'" type="checkbox">'+label+'</label></p>');
+  var $elem = $('<p><label for="'+id+'"><input id="'+id+'" type="checkbox"> '+label+'</label></p>');
   var $input = $elem.find('input');
   dd.bindGetSet($elem, $input.prop.bind($input, 'checked'));
   $input.on('change', dd.bindElement($elem, obj, ref, onChange));
